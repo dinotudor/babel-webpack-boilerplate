53c51dabdab4e27de54b2dd88477deac91dee121
Dino Tudor <dinotudor@gmail.com>
2019-08-30 11:32:24 +0200

    add class component

--- src/components/TechLIst.js
+++ src/components/TechLIst.js
@@ -2,16 +2,41 @@ import React, { Component } from 'react';
 
 class TechList extends Component {
   state = {
+    newTech: '',
     techs: ['Node.js', 'React.js', 'React Native']
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.newTech);
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ''
+    });
   };
 
   render() {
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <>
+        <h1>{this.state.newTech}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <ul>
+            {this.state.techs.map(tech => (
+              <li key={tech}>{tech}</li>
+            ))}
+          </ul>
+          <input
+            type="text"
+            onChange={this.handleInputChange}
+            value={this.state.newTech}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </>
     );
   }
 }
